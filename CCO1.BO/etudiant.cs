e4bd23d0fb1c6f968b9fb529e460efb38a7cc0d2
cedrdjio <69305726+cedrdjio@users.noreply.github.com>
2020-11-26 00:44:55 +0100

    le bon maintenant

--- CCO1.BO/etudiant.cs
+++ CCO1.BO/etudiant.cs
@@ -1,30 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CCO1.BO
 {
-    public class etudiant
+    [Serializable]
+    public class Etudiant
     {
         public string Nom { get; set; }
-        public string Prenom  { get; set; }
-        public string DateNaissance{ get; set; }
+        public string Prenom { get; set; }
+        public string DateNaissance { get; set; }
         public string DateAdmission { get; set; }
-        public string Matricule{ get; set; }
-        public string Departement{ get; set; }
+        public byte[] Matricule { get; set; }
+        public int Telephone  {get;set;}
+        public string  Emails{get;set;}
+        public string Etablissement {get;set;}
+         public string Departement { get; set; }
+        public byte[] Photo { get; set; }
+        public string Sex { get; set; }
 
 
-        public etudiant()
+        public Etudiant()
+        {
+
+        }
+        public Etudiant(string Nom)
         {
 
         }
 
-        public etudiant(string nom, string prenom,  long dateNaissance, string  dateAdmission, string matricule , string departement )
+        public Etudiant(string nom, string prenom, string dateNaissance, string dateAdmission, byte[] matricule, int telephone, string emails, string etablissement, string departement, byte[] photo, string sex) : this(nom)
         {
-            Name = name;
-            PostalCode = postalCode;
-            PhoneNumber = phoneNumber;
-            Email = email;
-            Logo = logo;
+            Prenom = prenom;
+            DateNaissance = dateNaissance;
+            DateAdmission = dateAdmission;
+            Matricule = matricule;
+            Telephone = telephone;
+            Emails = emails;
+            Etablissement = etablissement;
+            Departement = departement;
+            Photo = photo;
+            Sex = sex;
         }
 
+       
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Nom.Equals(etudiant.Nom, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return -1304721846 + EqualityComparer<string>.Default.GetHashCode(Nom);
+        }
     }
+
 }
+
+
